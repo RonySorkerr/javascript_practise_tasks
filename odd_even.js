98f03758bf59c03f430201470edb0375e566df22
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-05 13:31:44 +0600

    done

---
+++ odd_even.js
@@ -0,0 +1,12 @@
+// Take an integer value
+// Checks if it even or odd
+// If even returs even
+// If odd returns odd
+
+function odd_eve(values){
+
+}
+
+const number = 12;
+const result = odd(number);
+console.log('the result is : ' , result);
