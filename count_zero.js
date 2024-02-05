cf584ae55dbc5e50323fd08360fe2fec32b9cadf
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-05 13:27:59 +0600

    count character or number

--- count_zero.js
+++ count_zero.js
@@ -1,23 +1,45 @@
-// Take a binary string
-// make function to count how many 0 are there
-// 
+// // Take a binary string
+// // make function to count how many 0 are there
+// // 
 
 
 
-function count_zero(binaryString){
-    if(typeof binaryString !== 'string'){
-        return "please provie a string type value"
+// function count_zero(binaryString){
+//     if(typeof binaryString !== 'string'){
+//         return "please provie a string type value"
+//     }
+
+//     let zero = 0;
+//     for(const char of binaryString){
+//         if(char === '0'){
+//             zero++;
+//         }
+//     }
+//     return zero;
+// }
+
+// const binaryString = '101010100110001000010101010'
+// const zeros = count_zero(binaryString);
+// console.log('there is', zeros , 'zeros in the string');
+
+// COUNT A WORD IN A STRING
+
+function countChar(name){
+    if(typeof name !== 'string'){
+        return 'Please prove Your name';
     }
 
-    let zero = 0;
-    for(const char of binaryString){
-        if(char === '0'){
-            zero++;
+    let counts = 0;
+
+    for(const char of name.toLowerCase()){
+        if(char === 'r'){
+            counts++;
         }
     }
-    return zero;
+    return counts;
 }
 
-const binaryString = '101010100110001000010101010'
-const zeros = count_zero(binaryString);
-console.log('there is', zeros , 'zeros in the string');
+const name = 'Md.RrrrrronyRRRR Sorker';
+const result = countChar(name)
+console.log('there is ', result , 'times the char r is available');
+// console.log(name);
