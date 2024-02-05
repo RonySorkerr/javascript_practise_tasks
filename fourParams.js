394f05368356f7b2f82d2ccaf4c10e69c2c32658
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-05 12:41:25 +0600

    four params multiply

---
+++ fourParams.js
@@ -0,0 +1,11 @@
+// Take four parameters
+// Multiply the four numbers
+// Return the result
+
+function multiply(a, b, c,d){
+    const sum = a * b * c* d;
+    return sum;
+}
+
+const values = multiply(4,5,6,9);
+console.log(values);
