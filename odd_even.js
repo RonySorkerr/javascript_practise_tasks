1957269518cbc55905d116d02ba06e2b8d8844d6
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-05 13:33:47 +0600

    odd even

--- odd_even.js
+++ odd_even.js
@@ -4,9 +4,15 @@
 // If odd returns odd
 
 function odd_eve(values){
-
+    if(values % 2 === 0){
+        return 'EVEN';
+    }
+    else if(values % 2 !==0){
+        return 'ODD';
+    }
+    return "please provide a number";
 }
 
-const number = 12;
-const result = odd(number);
+const number = 15;
+const result = odd_eve(number);
 console.log('the result is : ' , result);
